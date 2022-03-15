1e30d447f763d84c7683545d23ced89b79c4795e
Artem Kastyuk <kastyuk.2016@gmail.com>
2022-03-15 15:04:17 +0300

    Some new information for class "Person"

--- university-kastyuk/src/com/belhard/university/Person.java
+++ university-kastyuk/src/com/belhard/university/Person.java
@@ -1,25 +1,54 @@
 package com.belhard.university;
 
+import java.time.LocalDate;
+
 public class Person {
 
-	public static void main(String[] args) {
+	private String firstName;
+	private String lastName;
+	private Address address;
+	
+	private LocalDate dateOfBirth;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	class Employee {
+		private String areaOfResponsibility;
+		
+		private int yearsOfStudy;
+		private int yearsOfExperience;
+		
+		private double salary;
+		
+		private boolean isBudget;
 
 	}
 
-}
+	class TeacherPerson extends Employee {
+		
+	}
 
-class Employee {
-	
-}
+	class Cleaner extends Employee{
+		
+	}
 
-class TeacherPerson extends Employee {
-	
-}
+	class StudentPerson{
+		
+	}
 
-class Cleaner extends Employee{
-	
-}
-
-class StudentPerson{
-	
 }
